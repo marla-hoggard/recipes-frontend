984546a2b91fcdedf32e1d1292dda5f4fb69aded
Marla Hoggard <marla@switchboard.app>
2026-05-24 22:03:57 -0400

    Upgrade node version + to vite

--- src/constants.ts
+++ src/constants.ts
@@ -13,12 +13,12 @@ export const USER_TOKEN_STORAGE_KEY = 'user_token';
 const getBackendURL = (env?: string) => {
   switch (env) {
     case 'local':
-      return process.env.REACT_APP_BACKEND_BASE_URL_LOCAL;
+      return import.meta.env.REACT_APP_BACKEND_BASE_URL_LOCAL;
     case 'ip':
-      return process.env.REACT_APP_BACKEND_BASE_URL_IP;
+      return import.meta.env.REACT_APP_BACKEND_BASE_URL_IP;
     default:
-      return process.env.REACT_APP_BACKEND_BASE_URL_PROD;
+      return import.meta.env.REACT_APP_BACKEND_BASE_URL_PROD;
   }
 };
 
-export const BACKEND_BASE_URL = process.env.REACT_APP_BACKEND_BASE_URL || getBackendURL();
+export const BACKEND_BASE_URL = import.meta.env.REACT_APP_BACKEND_BASE_URL || getBackendURL();
