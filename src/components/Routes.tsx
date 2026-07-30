3c88f1dbdc457fc00889dede54e93a392d4253a2
Marla Hoggard <marla@dittowords.com>
2026-07-30 02:03:16 -0400

    Clean up routes component

--- src/components/Routes.tsx
+++ src/components/Routes.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Routes as RRRoutes, Route, Navigate, useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-
 import { selectIsAuthenticated } from '../reducers/currentUser';
 import Header from './SiteFrame/Header/Header';
 import Main from './SiteFrame/Main/Main';
@@ -15,6 +14,48 @@ import RecipeView from './RecipeView/RecipeView';
 import SearchPage from './Search/SearchPage';
 import SignupForm from './Authentication/SignupForm';
 
+// /login and /signup are appended as a suffix to whatever page the user is currently on,
+// so they can't be expressed as ordinary react-router-dom route paths.
+const RouteContent: React.FC<{ isAuthenticated: boolean; pathname: string }> = ({
+  isAuthenticated,
+  pathname,
+}) => {
+  if (pathname.endsWith('/signup')) {
+    return isAuthenticated ? (
+      <Navigate to={pathname.replace('/signup', '')} replace />
+    ) : (
+      <SignupForm />
+    );
+  }
+
+  if (pathname.endsWith('/login')) {
+    return isAuthenticated ? (
+      <Navigate to={pathname.replace('/login', '')} replace />
+    ) : (
+      <LoginForm />
+    );
+  }
+
+  return (
+    <RRRoutes>
+      <Route path="/search" element={<SearchPage />} />
+      <Route path="/browse" element={<Browse />} />
+      <Route path="/browse/:name" element={<Browse />} />
+      <Route
+        path="/new"
+        element={isAuthenticated ? <AddRecipe /> : <Navigate to="/login" replace />}
+      />
+      <Route
+        path="/recipe/:id/edit"
+        element={isAuthenticated ? <EditRecipe /> : <Navigate to="/login" replace />}
+      />
+      <Route path="/recipe/:id" element={<RecipeView />} />
+      <Route path="/404" element={<InvalidRoute />} />
+      <Route path="*" element={<InvalidRoute />} />
+    </RRRoutes>
+  );
+};
+
 const Routes: React.FC = () => {
   const isAuthenticated = useSelector(selectIsAuthenticated);
   const { pathname } = useLocation();
@@ -23,56 +64,11 @@ const Routes: React.FC = () => {
     return <Homepage />;
   }
 
-  // /login and /signup are appended as a suffix to whatever page the user is currently on,
-  // so they can't be expressed as ordinary react-router-dom v6 route paths.
-  if (pathname.endsWith('/signup')) {
-    if (isAuthenticated) {
-      return <Navigate to={pathname.replace('/signup', '')} replace />;
-    }
-    return (
-      <>
-        <Header />
-        <Main>
-          <SignupForm />
-        </Main>
-      </>
-    );
-  }
-
-  if (pathname.endsWith('/login')) {
-    if (isAuthenticated) {
-      return <Navigate to={pathname.replace('/login', '')} replace />;
-    }
-    return (
-      <>
-        <Header />
-        <Main>
-          <LoginForm />
-        </Main>
-      </>
-    );
-  }
-
   return (
     <>
       <Header />
       <Main>
-        <RRRoutes>
-          <Route path="/search" element={<SearchPage />} />
-          <Route path="/browse" element={<Browse />} />
-          <Route path="/browse/:name" element={<Browse />} />
-          <Route
-            path="/new"
-            element={isAuthenticated ? <AddRecipe /> : <Navigate to="/login" replace />}
-          />
-          <Route
-            path="/recipe/:id/edit"
-            element={isAuthenticated ? <EditRecipe /> : <Navigate to="/login" replace />}
-          />
-          <Route path="/recipe/:id" element={<RecipeView />} />
-          <Route path="/404" element={<InvalidRoute />} />
-          <Route path="*" element={<InvalidRoute />} />
-        </RRRoutes>
+        <RouteContent isAuthenticated={isAuthenticated} pathname={pathname} />
       </Main>
     </>
   );
